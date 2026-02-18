57ce647768375d34779451ee2cbdbcdb9f53f748
Your Name <you@example.com>
2026-02-18 15:56:29 -0500

    Remove unused setSelectedUser import

--- src/sections/GrowthModeSection.tsx
+++ src/sections/GrowthModeSection.tsx
@@ -14,7 +14,6 @@ const GrowthModeSection: React.FC = () => {
     resetAssessment,
     currentUser,
     users,
-    setSelectedUser,
     setSelectedConversation,
     expressInterest,
     respondToInterest,
